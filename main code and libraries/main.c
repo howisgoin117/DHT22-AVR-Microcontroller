a9839570190c08e7a94f638ce3a6e494cd08e746
Phạm Tiến Mạnh <pham.tienmanh.school@gmail.com>
2025-04-04 17:48:55 +0700

    Update main.c

--- main code and libraries/main.c
+++ main code and libraries/main.c
@@ -1,3 +1,5 @@
+//VERSION 1: Running at 1MHz clock
+
 /*
 #define F_CPU 1000000UL
 #include <util/delay.h>
@@ -17,7 +19,6 @@ void init_ports() {
 	PORTD &= ~((1 << RED_LED) | (1 << GREEN_LED) | (1 << YELLOW_LED));
 }
 
-//conditions for turning LEDs on and off
 void control_led(double temp) {
 	if (temp < 24.0) {
 		PORTD |= (1 << RED_LED);
@@ -38,13 +39,13 @@ void control_led(double temp) {
 
 
 int main(void) {
-
 	DDRC = 0xFF;
 	DDRB = 0xFF;
 	init_ports();
 	LCD4_init();
 	DHT_Setup();
 	
+
 	while (1) {
 		char buffer[32];
 		double temperature;
@@ -105,12 +106,13 @@ int main(void) {
 
 */
 
+//VERSION 2: Running at 8 MHz
 //Updated code with UART 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#define F_CPU 1000000UL  //1 MHz clock
-#define BAUD 4800        
-#define MYUBRR ((F_CPU / (16UL * BAUD)) - 1)  //calculation for 4800 baud
+#define F_CPU 8000000UL  //1 MHz clock
+#define BAUD 9600
+#define MYUBRR ((F_CPU / (16UL * BAUD)) - 1)  //calculation for 9600 baud
 
 #include <util/delay.h>
 #include "DHT.h"
@@ -122,6 +124,7 @@ int main(void) {
 #define RED_LED PD3    // Red LED on PORTD, pin 3
 #define GREEN_LED PD4  // pin 4
 #define YELLOW_LED PD5 // pin 5
+#define WHITE_LED PD6  // Error indication LED at pin 6
 
 // UART initialization function for ATmega16A
 void UART_init(unsigned int ubrr) {
@@ -145,10 +148,11 @@ void UART_transmit_string(char* str) {
 }
 
 void init_ports() {
-	DDRD |= (1 << RED_LED) | (1 << GREEN_LED) | (1 << YELLOW_LED);
-	PORTD &= ~((1 << RED_LED) | (1 << GREEN_LED) | (1 << YELLOW_LED));
+	DDRD |= (1 << RED_LED) | (1 << GREEN_LED) | (1 << YELLOW_LED) | (1 << WHITE_LED);
+	PORTD &= ~((1 << RED_LED) | (1 << GREEN_LED) | (1 << YELLOW_LED) | (1 << WHITE_LED));
 }
 
+/*
 void control_led(double temp) {
 	if (temp < 24.0) {
 		PORTD |= (1 << RED_LED);
@@ -166,6 +170,7 @@ void control_led(double temp) {
 		PORTD &= ~(1 << GREEN_LED);
 	}
 }
+*/
 
 int main(void) {
 	DDRC = 0xFF;
@@ -176,7 +181,7 @@ int main(void) {
 	UART_init(MYUBRR);
 
 	// Send test message at startup
-	UART_transmit_string("System Started - ATmega16A @ 1MHz\r\n");
+	//UART_transmit_string("System Started - ATmega16A @ 8MHz\r\n");
 
 	while (1) {
 		char buffer[32];
@@ -184,7 +189,7 @@ int main(void) {
 		double humidity;
 		
 		DHT_Read(&temperature, &humidity);
-		control_led(temperature);
+		//control_led(temperature);
 		
 		LCD4_gotoxy(1, 1);
 		LCD4_write_string("Temperature:");
@@ -194,25 +199,55 @@ int main(void) {
 			case DHT_Ok:
 			sprintf(buffer, "%.1f C", temperature);
 			LCD4_write_string((unsigned char*)buffer);
-			
+			//UART
 			sprintf(buffer, "Temp: %.1f C, Hum: %.1f%%\r\n", temperature, humidity);
 			UART_transmit_string(buffer);
+			//LEDs control within switch loop 
+			if (temperature < 24.0) {
+				PORTD |= (1 << RED_LED);
+				} else {
+				PORTD &= ~(1 << RED_LED);
+			}
+			if (temperature >= 24.0 && temperature <= 26.0) {
+				PORTD |= (1 << YELLOW_LED);
+				} else {
+				PORTD &= ~(1 << YELLOW_LED);
+			}
+			if (temperature > 26.0) {
+				PORTD |= (1 << GREEN_LED);
+				} else {
+				PORTD &= ~(1 << GREEN_LED);
+			}
 			break;
 			case DHT_Error_Checksum:
 			LCD4_write_string("C Error");
 			UART_transmit_string("Checksum Error\r\n");
+			PORTD |= (1 << WHITE_LED);
 			break;
-			case DHT_Error_Timeout:
-			LCD4_write_string("T Error");
-			UART_transmit_string("Timeout Error\r\n");
+			case DHT_Error_Timeout1:
+			LCD4_write_string("T Error 1");
+			UART_transmit_string("Timeout Error Code 1\r\n");
+			PORTD |= (1 << WHITE_LED);
+			break;
+			case DHT_Error_Timeout2:
+			LCD4_write_string("T Error 2");
+			UART_transmit_string("Timeout Error Code 2\r\n");
+			PORTD |= (1 << WHITE_LED);
+			break;
+			case DHT_Error_Timeout3:
+			LCD4_write_string("T Error 3");
+			UART_transmit_string("Timeout Error Code 3\r\n");
+			PORTD |= (1 << WHITE_LED);
 			break;
 			case DHT_Error_Temperature:
 			LCD4_write_string("TR Error");
 			UART_transmit_string("Temperature Read Error\r\n");
+			PORTD |= (1 << WHITE_LED);
 			break;
 			default:
 			LCD4_write_string("Unknown Err");
 			UART_transmit_string("Unknown Error\r\n");
+			PORTD |= (1 << WHITE_LED);
 			break;
 		}
 		_delay_ms(2000);
